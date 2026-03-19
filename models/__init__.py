0b90ba964cd47147c114166fef25da727e54c05f
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2026-03-19 20:57:14 +0430

    new file added

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+# poultry_farm_management/models/__init__.py
+from . import employee
+from . import pharmacy_stock
+from . import cashier
+from . import partner
+from . import sale
+from . import purchase
+from . import pharmacy_payment
+from . import salary_payment
+from . import cash_account
+from . import cash_transfer
+from . import expense
+
